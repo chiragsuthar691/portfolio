cd6faab1fa23e793f8d5af2b3e47abbf01c88257
Chirag Suthar <65459147+chiragsuthar691@users.noreply.github.com>
2023-11-26 13:55:32 +0530

    updated about me

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -10,22 +10,21 @@ const AboutMe = () => {
         </div>
         <div className="mt-5 w-1/2 text-center about-desc">
           <p>
-            Back in 2012, I decided to try my hand at creating custom Tumblr
-            themes and tumbled head first into the rabbit hole of coding and web
-            development. Fast-forward to today, and I’ve had the privilege of
-            building software for an advertising agency, a start-up, a
-            student-led design studio, and a huge corporation
+            Skilled MERN Stack Developer with a proven track record in building
+            responsive web applications, optimizing performance and a relentless
+            commitment to continous learning for innovative solutions
           </p>
           <p>
             My main focus these days is building products and leading projects
-            for our clients at Upstatement. In my free time I&apos;ve also
-            released an online video course that covers everything you need to
-            know to build a web app with the Spotify API.
+            for our clients. In my free time I&apos;ve also created Instagram
+            automation system.
           </p>
           <p>
-            When I’m not at the computer, I’m usually rock climbing, hanging out
-            with my wife and two cats, or running around Hyrule searching for
-            Korok seeds K o r o k s e e d s .
+            a tech enthusiast who thrives in the digital realm. When I&apos;m
+            not crafting code, you&apos;ll find me on a yoga mat, enjoying
+            quality time with my amazing wife, and staying active. Balancing the
+            digital and real worlds is not just a philosophy for me; it&apos;s a
+            way of life. Welcome to my portfolio!
           </p>
         </div>
       </section>
